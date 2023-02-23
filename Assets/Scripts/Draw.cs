ed5f35fa9a364d049623fbc5429cf38d80b04d58
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2023-02-23 23:27:17 +0300

    v 0.7

---
+++ Assets/Scripts/Draw.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    [SerializeField] ObjectID objectID;
+    float timeForNextRay;
+    float timer = 0;
+    bool touchPlane;
+    public bool touchStartedOnPlayer;
+    Touch touch;
+
+    void Start()
+    {
+        touchStartedOnPlayer = false;
+        rb.isKinematic = true;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!touchStartedOnPlayer)
+        {
+            rb.isKinematic = false;
+            touchStartedOnPlayer = true;
+            touchPlane = true;
+            StartCoroutine(DrawEnum());
+        }
+    }
+
+    private IEnumerator DrawEnum()
+    {
+        while (touchStartedOnPlayer)
+        {
+            timer += Time.deltaTime;
+            if (Input.touchCount > 0 && GameManager.Instance.gameStat == GameManager.GameStat.start)
+            {
+                touch = Input.GetTouch(0);
+                switch (touch.phase)
+                {
+                    case TouchPhase.Moved:
+                        if (timer > timeForNextRay && touchStartedOnPlayer)
+                        {
+                            Vector3 worldFromMousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 100));
+                            Vector3 direction = worldFromMousePos - Camera.main.transform.position;
+                            RaycastHit hit;
+                            if (Physics.Raycast(Camera.main.transform.position, direction, out hit, 100f))
+                            {
+                                touchPlane = true;
+                                Vector3 pos = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                                transform.position = Vector3.Slerp(this.transform.position, pos, 10f);
+                                timer = 0;
+                            }
+                        }
+                        break;
+
+                    case TouchPhase.Ended:
+                        if (touchPlane)
+                        {
+                            touchStartedOnPlayer = false;
+                            touchPlane = false;
+                            rb.isKinematic = true;
+
+                            if (objectID.CheckAllObject() && objectID.objectStat == ObjectID.GameStat.isTouch)
+                            {
+                                transform.position = objectID.lastPos;
+                                objectID.objectStat = ObjectID.GameStat.isMove;
+                            }
+                        }
+                        break;
+                }
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
--- Assets/Scripts/ObjectID.cs
+++ Assets/Scripts/ObjectID.cs
@@ -12,9 +12,37 @@ public class ObjectID : MonoBehaviour
         isFinish = 3
     }
 
+    public int verticalCount, horizontalCount;
     public int ID;
     public int materialCount;
     public Vector3 lastPos;
     public GameStat objectStat;
     public List<ObjectID> broObject = new List<ObjectID>();
+
+    public bool CheckAllObject()
+    {
+        bool isCheck = false;
+
+        foreach (ObjectID item in broObject)
+            if (item.objectStat != GameStat.isPlacement) isCheck = true;
+
+        if (isCheck)
+            foreach (ObjectID item1 in broObject)
+                foreach (ObjectID item2 in broObject)
+                    if (item1.verticalCount == item2.verticalCount && item1.horizontalCount == item2.horizontalCount) isCheck = true;
+
+        return isCheck;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Grid"))
+        {
+            objectStat = GameStat.isPlacement;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+    }
 }
